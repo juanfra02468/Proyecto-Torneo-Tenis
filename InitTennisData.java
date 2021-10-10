df3b851d411357de8b1b817adf12ade31a338129
jgarciaroq <jgarciaroq@alumnos.unex.es>
2021-10-10 16:34:45 +0200

    TENISS DATA

--- InitTennisData.java
+++ InitTennisData.java
@@ -2,7 +2,7 @@
    /**
  * Clase que carga los datos con los que simular un campeonato de tenis
  * 
- * @author David Bonilla, Antonio Javier Hidalgo y Juan Francisco García
+ * @author David Bonilla, Antonio Javier Hidalgo y Juan Francisco García.
  * @version 1.0
  */
 public class InitTennisData {
