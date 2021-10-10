5e80e559670dd384e1a06939a2609bbf00e72321
antonio <jgarciaroq@alumnos.unex.es>
2021-10-10 16:36:35 +0200

    sdf

--- InitTennisData.java
+++ InitTennisData.java
@@ -10,7 +10,7 @@ public class InitTennisData {
 
     /**
      * Metodo que llama a la funcion cargar datos de un campeonato concreto
-     * @param Se le pasa el campeonato al que se hace referencia
+     * @param campeonato Se le pasa el campeonato al que se hace referencia
      */
     public InitTennisData(Campeonato campeonato) {
     //Descomentar la carga de datos que se quiera probar
@@ -20,7 +20,7 @@ public class InitTennisData {
     }
     /**
      * Metodo que se encarga de cargar los datos referentes a un campeonato concreto
-     * @param Se le pasa el campeonato al que se hace referencia
+     * @param campeonato Se le pasa el campeonato al que se hace referencia
      */
     private void cargarDatos1(Campeonato campeonato) {
         //Instanciamos 8 zapatillas: (Los parámetros están en el orden: modelo, número, tipo valor)
