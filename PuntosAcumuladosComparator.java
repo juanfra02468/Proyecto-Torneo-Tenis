996b779fa4b9d56674d9b33a489f3fe417c50144
Juanfra Garcia <jgarciaroq@alumnos.unex.es>
2021-10-10 19:08:05 +0200

    Reparacion

--- PuntosAcumuladosComparator.java
+++ PuntosAcumuladosComparator.java
@@ -10,7 +10,7 @@ class PuntosAcumuladosComparator implements Comparator<Tenista>{
     public int compare(Tenista t1, Tenista t2)
     {
         if(t1.getPuntosAcumulados()==t2.getPuntosAcumulados())
-            return 0;
+            return t1.getNombre().compareTo(t2.getNombre());
         else if (t1.getPuntosAcumulados()>t2.getPuntosAcumulados())
             return 1;
         else 
