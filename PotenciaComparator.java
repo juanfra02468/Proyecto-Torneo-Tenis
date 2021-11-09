0bd53dfa10bd445e90f05a57459660e90cd914dc
Juanfra Garcia <jgarciaroq@alumnos.unex.es>
2021-11-09 20:48:56 +0100

    Actualizacion3

--- PotenciaComparator.java
+++ PotenciaComparator.java
@@ -7,14 +7,22 @@
  */
 import java.util.*;  
 class PotenciaComparator implements Comparator<Raqueta>{  
-    public int compare(Raqueta r1, Raqueta r2)
-    {
-        if(r1.calcularPotencia(r1.getLongitud())==r2.calcularPotencia(r2.getLongitud()))
-            return r1.getModelo().compareTo(r2.getModelo());
-        else if (r1.calcularPotencia(r1.getLongitud())>r2.calcularPotencia(r2.getLongitud()))
-            return -1;
-        else 
-            return 1;
-    }
+       public int compare(Raqueta r1, Raqueta r2){
+        
+            if(r1.calcularPotencia(r1.getLongitud())==r2.calcularPotencia(r2.getLongitud()))
+                return r2.getModelo().compareTo(r1.getModelo());
+            else if (r1.calcularPotencia(r1.getLongitud())>r2.calcularPotencia(r2.getLongitud()))
+                return -1;
+            else 
+                return 1;
+        
+
+    } 
 
 }  
+
+
+
+
+
+
